f697c6f7c8afabf58eefe5f8f266bd2b621dc661
takuya <cdwbh564@yahoo.co.jp>
2024-12-03 16:23:20 +0900

    feature:ユーザー入力のバリデーション処理を実装 - サービス名等が未入力の場合、エラーメッセージを配列に追加 - エラーメッセージがあれば、全て出力 - 出力されるエラーメッセージは赤色に変更（視認性向上のため）

--- password_manager.sh
+++ password_manager.sh
@@ -8,4 +8,27 @@ read user_name
 echo -n 'パスワードを入力してください：'
 read -s password
 echo
-printf 'Thank you\033[31m!\033[0m\n'
+
+declare -a user_inputs=("$service_name" "$user_name" "$password")
+declare -a original_message=(
+  '\033[31mサービス名が入力されていません\033[0m\n'
+  '\033[31mユーザー名が入力されていません\033[0m\n'
+  '\033[31mパスワードが入力されていません\033[0m\n'
+)
+declare -a error_messages=()
+
+# 未入力項目を確認し、未入力の場合はエラーメッセージを追加
+for index in ${!user_inputs[@]}; do
+    if [ -z "${user_inputs[$index]}" ]; then
+        error_messages+=("${original_message[$index]}")
+    fi
+done
+
+# エラーが無い場合は感謝メッセージ出力、ある場合はエラーメッセージ出力、
+if [ -z "$error_messages" ]; then
+    printf 'Thank you\033[31m!\033[0m\n'
+else
+    for i in "${error_messages[@]}"; do
+        printf $i
+    done
+fi
