85f5c33edebe859711354cdfe57961bf60be2754
takuya <cdwbh564@yahoo.co.jp>
2024-12-06 17:39:28 +0900

    fix:処理に合わせてコメントを修正

--- password_manager.sh
+++ password_manager.sh
@@ -23,28 +23,29 @@ declare -a input_length_errors=(
 declare -a error_messages=()
 
 max_length=50
-# 未入力項目を確認し、未入力の場合はエラーメッセージを追加
+# 未入力項目と文字数超過を確認し、該当するエラーメッセージを配列に追加
 for index in ${!user_inputs[@]}; do
     if [ -z "${user_inputs[$index]}" ]; then
         error_messages+=("${original_message[$index]}")
     fi
-
+    
     if [ "${#user_inputs[index]}" -ge $this_max_length ]; then
         error_messages+=("${input_length_errors[$index]}")
     fi
 done
 
-# エラーが無い場合は感謝メッセージ出力、ある場合はエラーメッセージ出力、
+# エラーが無い場合、入力をファイルに保存
 if [ -z "$error_messages" ]; then
     (
         echo "$service_name":"$user_name":"$password" >> user_input.txt
     ) 2>error.txt
-
+    #保存失敗時、エラーメッセージを出力
     if [ $? -ne 0 ]; then
         printf '\033[31m入力内容の保存に失敗しました\033[0m\n'
         return
     fi
     printf 'Thank you\033[31m!\033[0m\n'
+# エラーがある場合、エラーメッセージ出力
 else
     for i in "${error_messages[@]}"; do
         printf $i
